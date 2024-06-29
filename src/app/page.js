277156a29c71ca59d780921df907c7183cb2fbc0
redfit <nagita.0724.0812@gmail.com>
2024-06-29 11:41:57 +0900

    List Available Chat Rooms in Real-time in Next.js from Fauna

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,55 @@
 "use client";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Client, fql } from "fauna";
 
 export default function Home() {
   const [roomName, setRoomName] = useState("");
+  const [existingRooms, setExistingRooms] = useState([]);
 
+  const streamRef = useRef(null);
   const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+
+  const fetchRooms = async () => {
+    try {
+      const response = await client.query(fql`
+        Room.all()
+      `);
+      setExistingRooms(response.data.data);
+      if (!streamRef.current) {
+        const stream = await client.stream(fql`Room.all().toStream()`);
+        streamRef.current = stream;
+        for await (const event of stream) {
+          switch (event.type) {
+            case "add":
+            case "update":
+              setExistingRooms((prev) => {
+                const existingRoom = prev.findIndex(
+                  (room) => room.id === event?.data.id,
+                );
+                if (existingRoom === -1) {
+                  return [...prev, event.data];
+                } else {
+                  return prev.map((room) => {
+                    if (room.id === event?.data.id) {
+                      return event.data;
+                    }
+                    return room;
+                  });
+                }
+              });
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!client) return;
+    fetchRooms();
+  }, []);
 
   const createNewRoom = async (e) => {
     e.preventDefault();
@@ -16,7 +59,6 @@ export default function Home() {
         name: ${roomName},
         })
       `);
-      console.log(newroom);
     } catch (error) {
       console.log(error);
     }
@@ -34,6 +76,14 @@ export default function Home() {
           />
           <button type="submit">Create room</button>
         </form>
+      </div>
+      <div>
+        <h2>Existing rooms</h2>
+        {existingRooms.map((room) => (
+          <div key={room.id}>
+            <button>{room.name}</button>
+          </div>
+        ))}
       </div>
     </main>
   );
