7fe318c7b912af7d94484ce09e9fdca4dee3cc0a
redfit <nagita.0724.0812@gmail.com>
2024-06-29 11:12:42 +0900

    Create a Fauna Room Collection in Next.js with the fauna Client

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
 "use client";
 import styles from "./page.module.css";
 import { useState } from "react";
+import { Client, fql } from "fauna";
 
 export default function Home() {
   const [roomName, setRoomName] = useState("");
 
-  const createNewRoom = async (e) => {};
+  const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+
+  const createNewRoom = async (e) => {
+    e.preventDefault();
+    try {
+      const newroom = await client.query(fql`
+        Room.create({
+        name: ${roomName},
+        })
+      `);
+      console.log(newroom);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <main className={styles.main}>
