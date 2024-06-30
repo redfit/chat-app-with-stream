5820e5de4fb41f67ee7dd61527f9a7aaa0cf00fc
redfit <nagita.0724.0812@gmail.com>
2024-06-30 16:22:47 +0900

    Implement FAuna Signin with Next.js Server Actions and useFormState React hook

---
+++ src/app/actions.js
@@ -0,0 +1,26 @@
+"use server";
+
+import { Client, fql } from "fauna";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function doSignin(prevState, formData) {
+  const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+  const email = formData.get("email");
+  const password = formData.get("password");
+  if (!email || !password) {
+    return { message: "Email and password are required" };
+  }
+  try {
+    const response = await client.query(fql`
+      UserLogin(${email}, ${password})
+    `);
+    console.log(response);
+    cookies().set("chat-app", response.data.secret);
+  } catch (error) {
+    console.log(error);
+    return { message: "An error occurred" };
+  }
+
+  redirect("/");
+}
--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,14 +1,23 @@
-export default function SigninPage() {
-  const doSignin = () => {};
+"use client";
 
+import { useFormState } from "react-dom";
+import { doSignin } from "@/app/actions";
+
+const initialState = {
+  message: "",
+};
+
+export default function SigninPage() {
+  const [state, formAction] = useFormState(doSignin, initialState);
   return (
     <div>
       <h1>Signin Page</h1>
-      <form action={doSignin}>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+      <form action={formAction}>
+        <input type="text" placeholder="Email" name="email" />
+        <input type="password" placeholder="Password" name="password" />
         <button type="submit">Signin</button>
       </form>
+      {state.message && <p>{state.message}</p>}
     </div>
   );
 }
