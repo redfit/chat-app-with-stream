baef19de5671684c53c4c5d1d1fa229c51eac348
redfit <nagita.0724.0812@gmail.com>
2024-06-30 11:19:36 +0900

    Implement Signup Functionality through Fauna with Next.js Server Actions

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,13 +1,33 @@
+import { Client, fql } from "fauna";
+import { redirect } from "next/navigation";
 export default function SignupPage() {
-  const doSignup = () => {};
+  const doSignup = async (formData) => {
+    "use server";
+    const client = new Client({
+      secret: process.env.NEXT_PUBLIC_FAUNA_KEY,
+    });
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      await client.query(fql`
+        UserRegistration(${username}, ${email}, ${password})
+      `);
+    } catch (error) {
+      console.error(error);
+    }
+
+    redirect("/signin");
+  };
 
   return (
     <div>
       <h1>Signup Page</h1>
       <form action={doSignup}>
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Username" name="username" />
+        <input type="text" placeholder="Email" name="email" />
+        <input type="password" placeholder="Password" name="password" />
         <button type="submit">Signup</button>
       </form>
     </div>
